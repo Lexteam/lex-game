2140bf0475b9a00dfb93aff94369e7d2900fa68c
Tom <tomdr1001@outlook.com>
2015-04-25 16:51:03 +0100

    Connection stuff.

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/Connection.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/Connection.java
@@ -7,11 +7,26 @@ package uk.jamierocks.lexteam.ygd.core.objects;
  */
 public class Connection extends GameObject {
 
+    /**
+     * Represents the point the connection goes to
+     */
     public Point pointTo;
+
+    /**
+     * Represents the point the connection goes from
+     */
     public Point pointFrom;
 
     public Connection(Point PointTo, Point PointFrom) {
         this.pointTo = PointTo;
         this.pointFrom = PointFrom;
+    }
+
+    /**
+     * @param connection the connection to compare to
+     * @return Returns true if the connection contains a point that this connection also contains
+     */
+    public boolean isConnectedTo(Connection connection) {
+        return connection.pointFrom == this.pointFrom || connection.pointTo == this.pointTo;
     }
 }
