7e0af994adebe2fbdb5c74fd04fe3c431ba89716
Tom <tomdr1001@outlook.com>
2015-04-25 12:11:30 +0100

    Started filling in section interface.

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/section/Section.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/section/Section.java
@@ -1,10 +1,28 @@
 package uk.jamierocks.lexteam.ygd.core.section;
 
+import uk.jamierocks.lexteam.ygd.core.level.Level;
+
+import java.util.Set;
+
 /**
  * Represents a section
  *
  * @author Jamie Mansfield
+ * @author Tom Dreverz
  */
-public class Section {
+public interface Section {
 
+    /**
+     * Collection of levels
+     *
+     * @return all levels
+     */
+    Set<Level> getLevels();
+
+    /**
+     * Adds a level/multiple levels to the Levels collection
+     *
+     * @param levels the level/s to be added
+     */
+    void addLevel(Level... levels);
 }
