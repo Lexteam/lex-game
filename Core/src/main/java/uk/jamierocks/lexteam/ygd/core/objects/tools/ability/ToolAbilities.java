a66cb9461d80c0d32b7f815c05e36f5ff1b41879
Jamie Mansfield <malgm@lapis.blue>
2015-05-02 23:32:59 +0100

    Scrap all old classes, in favour of new nicer system

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/tools/ability/ToolAbilities.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/tools/ability/ToolAbilities.java
@@ -10,30 +10,60 @@ public final class ToolAbilities {
     /**
      * A class representing the ToolAbility to add points to a connection
      */
-    public static final ToolAbility ADD_POINT = new ToolAbilityAddPoint();
+    public static final ToolAbility ADD_POINT = new ToolAbility() {
+        @Override
+        public void use() {
+
+        }
+    };
 
     /**
      * A class representing for the ToolAbility to change the frequency of the connection
      */
-    public static final ToolAbility CHANGE_CONNECTION_FREQUENCY = new ToolAbilityChangeConnectionFrequency();
+    public static final ToolAbility CHANGE_CONNECTION_FREQUENCY = new ToolAbility() {
+        @Override
+        public void use() {
+
+        }
+    };
 
     /**
      * A class representing for the ToolAbility to change the direction of the connection
      */
-    public static final ToolAbility CHANGE_DIRECTION = new ToolAbilityChangeDirection();
+    public static final ToolAbility CHANGE_DIRECTION = new ToolAbility() {
+        @Override
+        public void use() {
+
+        }
+    };
 
     /**
      * A class representing for the ToolAbility to merge two connections into one
      */
-    public static final ToolAbility MERGE_CONNECTIONS = new ToolAbilityMergeConnections();
+    public static final ToolAbility MERGE_CONNECTIONS = new ToolAbility() {
+        @Override
+        public void use() {
+
+        }
+    };
 
     /**
      * A class representing for the ToolAbility to remove a point in a connection
      */
-    public static final ToolAbility REMOVE_POINT = new ToolAbilityRemovePoint();
+    public static final ToolAbility REMOVE_POINT = new ToolAbility() {
+        @Override
+        public void use() {
+
+        }
+    };
 
     /**
      * A class representing for the ToolAbility to split a connection into two new connections
      */
-    public static final ToolAbility SPLIT_CONNECTION = new ToolAbilitySplitConnection();
+    public static final ToolAbility SPLIT_CONNECTION = new ToolAbility() {
+        @Override
+        public void use() {
+
+        }
+    };
 }
--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/tools/ability/ToolAbility.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/tools/ability/ToolAbility.java
@@ -6,7 +6,12 @@ import uk.jamierocks.lexteam.ygd.core.objects.tools.Tool;
  * Represents a {@link Tool}'s ability
  *
  * @author Tom Drever
+ * @author Jamie Mansfield
  */
 public interface ToolAbility {
 
+    /**
+     * Called when a {@link Tool} is used
+     */
+    void use();
 }
