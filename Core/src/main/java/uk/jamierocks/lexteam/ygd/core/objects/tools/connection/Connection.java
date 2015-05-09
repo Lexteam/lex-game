130cdaa377910a664b772d66be1b7fd4f74eede3
Jamie Mansfield <malgm@lapis.blue>
2015-05-09 19:06:26 +0100

    Ich liebe Informatik

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/tools/connection/Connection.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/objects/tools/connection/Connection.java
@@ -3,14 +3,13 @@ package uk.jamierocks.lexteam.ygd.core.objects.tools.connection;
 import uk.jamierocks.lexteam.ygd.core.objects.GameObject;
 import uk.jamierocks.lexteam.ygd.core.objects.Point;
 
-import java.util.*;
-
 /**
  * Class to represent the concept of a connection. Contains a PointFrom and PointTo
  *
  * @author Tom Drever
+ * @author Jamie Mansfield
  */
-     public class Connection extends GameObject {
+public class Connection extends GameObject {
 
      /**
      * Represents the point the connection goes to
@@ -27,41 +26,29 @@ import java.util.*;
      */
     public ConnectionFrequency connectionFrequency;
 
-    public Connection(Point PointTo, Point PointFrom, ConnectionFrequency Frequency) {
-        this.pointTo = PointTo;
-        this.pointFrom = PointFrom;
-        this.connectionFrequency = Frequency;
+    public Connection(Point pointTo, Point pointFrom, ConnectionFrequency frequency) {
+        this.pointTo = pointTo;
+        this.pointFrom = pointFrom;
+        this.connectionFrequency = frequency;
     }
 
     /**
+     * Checks if this Connection is connected to another Connection
+     *
      * @param connection the connection to compare to
-     * @return Returns true if the connection contains a point that this connection also contains
+     * @return {@code true} if the connection contains a point that this connection also contains
      */
     public boolean isConnectedTo(Connection connection) {
         return connection.connectionFrequency == this.connectionFrequency && (connection.pointFrom == this.pointFrom || connection.pointTo == this.pointTo);
     }
 
     /**
-     * @param ID The ID of the point to be checked
-     * @return Returns true if the point is found within this connection
+     * Checks is this Connection contains a Point
+     *
+     * @param ID the ID of the point to be checked
+     * @return {@code true} if the point is found within this connection
      */
     public boolean hasPoint(int ID){
         return this.pointTo.ID == ID || this.pointFrom.ID == ID;
-    }
-
-    /**
-     * @return Returns the amount of points in this connection that are not null
-     */
-    public Set<Point> configuredPoints(){
-        Set<Point> configuredPoints = new HashSet<>();
-
-        if (this.pointTo != null){
-            configuredPoints.add(this.pointTo);
-        }
-        if (this.pointFrom != null){
-            configuredPoints.add(this.pointFrom);
-        }
-
-        return configuredPoints;
     }
 }
