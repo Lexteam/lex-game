a8ed04081a98990a533e3b8453227e8bf74607d3
Jamie Mansfield <malgm@lapis.blue>
2015-06-08 17:38:29 +0100

    SFML and PushButtonWrapper

--- Implementation/src/main/java/uk/jamierocks/lexteam/ygd/game/GameLaunch.java
+++ Implementation/src/main/java/uk/jamierocks/lexteam/ygd/game/GameLaunch.java
@@ -1,11 +1,21 @@
 package uk.jamierocks.lexteam.ygd.game;
 
+import uk.jamierocks.lexteam.ygd.core.Game;
+import uk.jamierocks.lexteam.ygd.core.YGDGame;
+
 /**
+ * The implementation of {@link Game}.
+ *
  * @author Jamie Mansfield
  */
-public class GameLaunch {
+public class GameLaunch extends Game {
 
     public static void main(String[] args) {
+        YGDGame.setGame(new GameLaunch());
+    }
+
+    @Override
+    public void shutdown() {
 
     }
 }
