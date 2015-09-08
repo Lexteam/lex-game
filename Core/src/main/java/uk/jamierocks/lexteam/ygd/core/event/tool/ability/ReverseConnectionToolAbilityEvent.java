26aa411446ebc89ba07631cb31610fe46459d7f2
Jamie Mansfield <malgm@lapis.blue>
2015-09-08 21:26:35 +0100

    Clear out Core in preperation for rewrite

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/event/tool/ability/ReverseConnectionToolAbilityEvent.java
+++
@@ -1,8 +0,0 @@
-package uk.jamierocks.lexteam.ygd.core.event.tool.ability;
-
-/**
- * Created by Jamie_2 on 16/05/2015.
- */
-public class ReverseConnectionToolAbilityEvent {
-
-}
--- src/main/java/uk/jamierocks/lexteam/ygd/game/GameLaunch.java
+++ src/main/java/uk/jamierocks/lexteam/ygd/game/GameLaunch.java
@@ -1,26 +1,12 @@
 package uk.jamierocks.lexteam.ygd.game;
 
-import uk.jamierocks.lexteam.ygd.core.Game;
-import uk.jamierocks.lexteam.ygd.core.YGDGame;
-
 /**
  * The implementation of {@link Game}.
  *
  * @author Jamie Mansfield
  */
-public class GameLaunch extends Game {
+public class GameLaunch {
 
     public static void main(String[] args) {
-        YGDGame.setGame(new GameLaunch());
-    }
-
-    @Override
-    protected void init() {
-        YGDGame.defaultInit();
-    }
-
-    @Override
-    public void shutdown() {
-
     }
 }
