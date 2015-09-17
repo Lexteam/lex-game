808816fadef9a589015900146568cf67a278fef1
Jamie Mansfield <malgm@lapis.blue>
2015-09-17 16:47:44 +0100

    javadoc Path API

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/path/Path.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/path/Path.java
@@ -9,11 +9,31 @@ import uk.jamierocks.lexteam.ygd.core.panel.BasePanel;
  */
 public interface Path {
 
+    /**
+     * Gets the first panel in the circuit.
+     *
+     * @return the starting panel.
+     */
     BasePanel getStart();
 
+    /**
+     * Gets the last panel in the circuit.
+     *
+     * @return the finishing panel.
+     */
     BasePanel getFinish();
 
+    /**
+     * Sees if the circuit is complete.
+     *
+     * @return {@code true} if the circuit is complete.
+     */
     boolean isComplete();
 
+    /**
+     * Gets the time it takes for the pulse, to get from the starting position to the end or the circuit.
+     *
+     * @return the pulse time.
+     */
     int getPulseTime();
 }
--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/path/Paths.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/path/Paths.java
@@ -2,6 +2,11 @@ package uk.jamierocks.lexteam.ygd.core.path;
 
 import uk.jamierocks.lexteam.ygd.core.section.level.Level;
 
+/**
+ * Static access to methods required to make paths.
+ *
+ * @author Jamie Mansfield
+ */
 public final class Paths {
 
     public static Path of(Level level) {
