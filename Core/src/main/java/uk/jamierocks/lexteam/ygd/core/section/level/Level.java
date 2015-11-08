a37dbbab3d88f3080bbfecd5ebacfaea664779df
charlesstewart <tomdr1001@outlook.com>
2015-11-08 16:57:54 +0000

    Level thingy.

--- Core/src/main/java/uk/jamierocks/lexteam/ygd/core/section/level/Level.java
+++ Core/src/main/java/uk/jamierocks/lexteam/ygd/core/section/level/Level.java
@@ -16,7 +16,6 @@ import java.util.Set;
  * Represents a level, containing the panels
  *
  * @author Tom Drever
- * @author Jamie Mansfield
  */
 public class Level {
 
@@ -66,49 +65,51 @@ public class Level {
                     else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM){
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getLeft()), new Vector2f(panel.getCubePosition().getVector2f().getY(), 0));
                     }
-                    adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getLeft()), new Vector2f(panelsPerFace - 1, panel.getCubePosition().getVector2f().getY()));
+                    else{
+                        adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getLeft()), new Vector2f(panelsPerFace - 1, panel.getCubePosition().getVector2f().getY()));
+                    }
                 }
                 else {
                     adjacentPanelCubePosition = new CubePosition(panel.getCubePosition().getCubeFace(),new Vector2f(panel.getCubePosition().getVector2f().getX() - 1,panel.getCubePosition().getVector2f().getY()));
                 }
                 break;
             case RIGHT:
-                if (panel.getCubePosition().getVector2f().getX() == panelsPerFace - 1){
-                    if (panel.getCubePosition().getCubeFace() == CubeFace.TOP){
+                if (panel.getCubePosition().getVector2f().getX() == panelsPerFace - 1) {
+                    if (panel.getCubePosition().getCubeFace() == CubeFace.TOP) {
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getRight()), new Vector2f(panel.getCubePosition().getVector2f().getY(), panelsPerFace - 1));
-                    }
-                    else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM){
+                    } else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM) {
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getRight()), new Vector2f(panel.getCubePosition().getVector2f().getY(), 0));
+                    } else {
+                        adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getRight()), new Vector2f(panelsPerFace - 1, panel.getCubePosition().getVector2f().getY()));
                     }
-                    adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getRight()), new Vector2f(panelsPerFace - 1, panel.getCubePosition().getVector2f().getY()));
                 }
                 else {
                     adjacentPanelCubePosition = new CubePosition(panel.getCubePosition().getCubeFace(),new Vector2f(panel.getCubePosition().getVector2f().getX() + 1,panel.getCubePosition().getVector2f().getY()));
                 }
                 break;
             case UP:
-                if (panel.getCubePosition().getVector2f().getY() == panelsPerFace - 1){
-                    if (panel.getCubePosition().getCubeFace() == CubeFace.TOP){
+                if (panel.getCubePosition().getVector2f().getY() == panelsPerFace - 1) {
+                    if (panel.getCubePosition().getCubeFace() == CubeFace.TOP) {
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(panelsPerFace - 1, panel.getCubePosition().getVector2f().getY()));
-                    }
-                    else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM){
+                    } else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM) {
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(0, panel.getCubePosition().getVector2f().getX()));
+                    } else {
+                        adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(panel.getCubePosition().getVector2f().getX(), panelsPerFace - 1));
                     }
-                    adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(panel.getCubePosition().getVector2f().getX(), panelsPerFace - 1));
                 }
                 else {
                     adjacentPanelCubePosition = new CubePosition(panel.getCubePosition().getCubeFace(),new Vector2f(panel.getCubePosition().getVector2f().getX(), panel.getCubePosition().getVector2f().getY() + 1));
                 }
                 break;
             case DOWN:
-                if (panel.getCubePosition().getVector2f().getY() == 0){
-                    if (panel.getCubePosition().getCubeFace() == CubeFace.TOP){
+                if (panel.getCubePosition().getVector2f().getY() == 0) {
+                    if (panel.getCubePosition().getCubeFace() == CubeFace.TOP) {
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(panelsPerFace - 1, panel.getCubePosition().getVector2f().getY()));
-                    }
-                    else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM){
+                    } else if (panel.getCubePosition().getCubeFace() == CubeFace.BOTTOM) {
                         adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(0, panel.getCubePosition().getVector2f().getX()));
+                    } else {
+                        adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(panel.getCubePosition().getVector2f().getX(), 0));
                     }
-                    adjacentPanelCubePosition = new CubePosition(CubeFace.getCubeFace(panel.getCubePosition().getCubeFace().getUp()), new Vector2f(panel.getCubePosition().getVector2f().getX(), 0));
                 }
                 else {
                     adjacentPanelCubePosition = new CubePosition(panel.getCubePosition().getCubeFace(),new Vector2f(panel.getCubePosition().getVector2f().getX(), panel.getCubePosition().getVector2f().getY() - 1));
